dad2a818c42b63125a28d09d394b34e9bfb4c75b
MinJeung-Kim <focso5@gmail.com>
2023-12-13 19:13:06 +0900

    Fix: List

--- client/src/components/Member/List/List.tsx
+++ client/src/components/Member/List/List.tsx
@@ -1,33 +1,22 @@
-import { UserResponse } from "service/auth";
+import { Member } from "pages/Member/Member";
 
 import styles from "./List.module.css";
 
 type Props = {
-  members: UserResponse[];
+  members: Member[];
 };
 
 const COLUMNS = [
-  { field: "name", header: "ID" },
-  { field: "username", header: "이름" },
+  { field: "username", header: "ID" },
+  { field: "name", header: "이름" },
   { field: "phone", header: "연락처" },
   { field: "email", header: "이메일" },
   { field: "paymentTerm", header: "기간" },
   { field: "extendedPeriod", header: "연장" },
-  { field: "totalPeriod", header: "총 기간" },
-  { field: "locker", header: "사물함" },
-  { field: "createdAt", header: "가입일" },
+  { field: "createdAt", header: "등록일" },
 ];
 
 export default function List({ members }: Props) {
-  // const displayMembers = () => {
-  //   return (
-  //     <ul className={styles.members}>
-  //       {members.map((member) =>
-  //         COLUMNS.map((col) => <li key={col.header}>{member[col.field]}</li>)
-  //       )}
-  //     </ul>
-  //   );
-  // };
   return (
     <div className={styles.wrapper}>
       <ul className={styles.columns}>
@@ -38,9 +27,7 @@ export default function List({ members }: Props) {
       {/* <div className={styles.none_data}>데이터가 없습니다.</div> */}
       <ul className={styles.members}>
         {members.map((member) =>
-          COLUMNS.map((col) => (
-            <li key={col.header}>{(member as any)[col.field]}</li>
-          ))
+          COLUMNS.map((col) => <li key={col.header}>{member[col.field]}</li>)
         )}
       </ul>
     </div>
